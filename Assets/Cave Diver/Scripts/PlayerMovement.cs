6ecfabdd09a2fd75a6cda2d7af4ba50f1a0ddf60
SteveoBurns <steveo.burns.777@gmail.com>
2021-03-31 13:01:56 +1100

    Initial commit

---
+++ Assets/Cave Diver/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 5f;
+    [SerializeField] private float diveSpeed = 10f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        transform.position += Vector3.right * playerSpeed * Time.fixedDeltaTime;
+        transform.position += Vector3.up * playerSpeed * Time.fixedDeltaTime;
+
+        if (Input.GetButton("Dive"))
+        {
+            transform.position += Vector3.down * diveSpeed * Time.fixedDeltaTime;
+        }
+
+        
+       
+    }
+   
+}
