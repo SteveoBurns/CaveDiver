529c95ff457b41b7e356c65f6f5234b2b9e60b3e
SteveoBurns <steveo.burns.777@gmail.com>
2021-04-28 10:18:44 +1000

    Added explosion sound

--- Assets/Cave Diver/Scripts/Mine.cs
+++ Assets/Cave Diver/Scripts/Mine.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Mine : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private AudioSource explosion;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         animator.SetTrigger("OnHit");
+        explosion.Play();
     }    
 }
